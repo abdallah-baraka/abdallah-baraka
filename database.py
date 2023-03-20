8bd993ab28645a8f6f3fadcf1ab60ad19b634f36
abdallah-baraka <92726144+abdallah-baraka@users.noreply.github.com>
2023-03-20 12:06:11 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from  sqlalchemy.orm import sessionmaker
-engine=create_engine("mysql://4276136_4276136:abdallah_service2023@fdb1029.awardspace.net/4276136_4276136")
+engine=create_engine("mysql://4276136_4276136:abdallah_service2023@http://fdb1029.awardspace.net/4276136_4276136")
 
 base=declarative_base()
 sessionlocal=sessionmaker(bind=engine)
